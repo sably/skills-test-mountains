79ab7127495c05049b7e9014eb0d2f9e62fd0cc4
sably <massdownup@gmail.com>
2023-08-15 13:10:40 +0330

    fixed some bug

--- hooks/useMedia.tsx
+++ hooks/useMedia.tsx
@@ -3,18 +3,21 @@
 import { useState, useEffect } from "react";
 
 const useMedia = (query: string): boolean => {
-  const [matches, setMatches] = useState(window.matchMedia(query).matches);
+  const isClient = typeof window === "object";
+  const [matches, setMatches] = useState(
+    isClient && window.matchMedia(query).matches
+  );
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const media = window.matchMedia(query);
-      if (media.matches !== matches) setMatches(media.matches);
-      const listener = () => setMatches(media.matches);
-      media.addEventListener("change", listener);
+    if (!isClient) return;
 
-      return () => media.removeEventListener("change", listener);
-    }
-  }, [matches, query]);
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) setMatches(media.matches);
+    const listener = () => setMatches(media.matches);
+    media.addEventListener("change", listener);
+
+    return () => media.removeEventListener("change", listener);
+  }, [isClient, matches, query]);
 
   return matches;
 };
